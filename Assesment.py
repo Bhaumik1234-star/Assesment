d988aa82ba4565c53c684485c5db4d68033ef564
Bhaumik1234-star <bhaumikrathore@riccarton.school.nz>
2025-03-26 14:01:07 +1300

    Assement 3

--- Assesment.py
+++ Assesment.py
@@ -4,9 +4,18 @@ and if the power level is more than 12 its mean its not safe'''
 # Ask Enter the dugeon name 
 adventurer = input('Enter the dungeon name: ')
 # Ask Enter the dugeon level 
-adventurer = float(input('Enter the dugeon level: '))
+level = float(input('Enter the dugeon level: '))
+if level > 0:
+    return level
+else:
+    print('"Invalid input. The dungeon level must be a positive integer.')
 
-if adventurer <= 12: 
+except ValueError:
+            print("Invalid input. Please enter a positive integer.")
+
+if level <= 12: 
     print(f'We can fearlessly explore {adventurer}')
 else:
      print(f'We cannot fearlessly explore {adventurer}')
+
+    
