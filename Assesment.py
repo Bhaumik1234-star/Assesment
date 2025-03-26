0fe307744d544c4c4e31ee39fb4b1ab933656e35
Bhaumik1234-star <bhaumikrathore@riccarton.school.nz>
2025-03-26 14:11:04 +1300

    Assesment 4

--- Assesment.py
+++ Assesment.py
@@ -1,21 +1,26 @@
 ''' This program asks to figure which dugeons are safe to explore and if power level is less than 12 or equal to 12 its mean safe
 and if the power level is more than 12 its mean its not safe'''
 
-# Ask Enter the dugeon name 
-adventurer = input('Enter the dungeon name: ')
 # Ask Enter the dugeon level 
 level = float(input('Enter the dugeon level: '))
 if level > 0:
     return level
 else:
-    print('"Invalid input. The dungeon level must be a positive integer.')
+    print('Invalid input. The dungeon level must be a positive integer.')
 
-except ValueError:
-            print("Invalid input. Please enter a positive integer.")
+    except ValueError:
+        print("Invalid input. Please enter a positive integer.")
 
-if level <= 12: 
-    print(f'We can fearlessly explore {adventurer}')
-else:
-     print(f'We cannot fearlessly explore {adventurer}')
+def main():
+    party_level = 12 
+    dugeonus = []
+
+    while True:
+        dugeonus_name = input('Enter the dugeon name: ')
+        dugeonus_level = get_valid_dugeouns_level()
+        dugeonus.append((dugeonus_name, dugeonus_level))
+        if dugeon level == 'Delfino Square':
+            break
+        
 
     
